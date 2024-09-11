f16e37fa7704d164b21aad12eef6774aa4eded72
ahmnaseif <91281643+ahmnaseif@users.noreply.github.com>
2024-09-11 23:35:25 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -18,7 +18,7 @@ def get_llm_response(question):
 
     # Load the LLM model using CTransformers
         model = CTransformers(
-        model='meta-llama/Meta-Llama-3.1-8B', 
+        model='model/llama-2-7b-chat.ggmlv3.q2_K.bin', 
         model_type='llama'
         )
         print("Question: ")
